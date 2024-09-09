75eeafb9030f71ede5a4f285e87e7d7a59a1d515
Dreako2535 <brayan.alzate.923@gmail.com>
2024-09-09 16:35:19 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Heder from "./componentes/heder"
+
+function App() {
+  return (
+    <div className="App">
+      <Heder />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/heder.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+function heder() {
+    
+}
+
+export default heder;
